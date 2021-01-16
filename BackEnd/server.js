bd5536c4d18eb9b6596b02d9b6b6400b8ac44eeb
hit3sh2000 <69767067+hit3sh2000@users.noreply.github.com>
2021-01-16 12:28:59 +0530

    added basic user CRUD OP

---
+++ BackEnd/server.js
@@ -0,0 +1,21 @@
+require('./models/db');                       //import MONGODB connction files
+const express = require('express');                      // import  express
+const bodyparser = require('body-parser');               // import  body-parser                    
+const user = require('./routes/userRoute');    // import  userController                        
+const app = express()                                    //Asigning express               
+
+app.use(bodyparser.urlencoded({
+    extended: true                          //Asigning bodyparser
+}));
+app.use(bodyparser.json());                   //Asigning json of bodyparser
+
+app.get('/', (req, res) => {
+  res.send('Hello World');                        //simple route for hello World
+});
+
+
+app.use('/user',user);        //  setting router 
+
+const PORT = 3000;
+app.listen(PORT,console.log(`Port is running on http://localhost:${PORT}`));
+
