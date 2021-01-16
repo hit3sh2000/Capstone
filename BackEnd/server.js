b83c7467ce4c8263c540eb63085c92f0b227cefa
SurajSS007 <surajsuryawanshi220@gmail.com>
2021-01-16 13:54:20 +0530

    adding course route to existing

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -2,6 +2,7 @@ require('./models/db');                       //import MONGODB connction files
 const express = require('express');                      // import  express
 const bodyparser = require('body-parser');               // import  body-parser                    
 const user = require('./routes/userRoute');    // import  userController                        
+const course = require('./routes/courseRoute')
 const app = express()                                    //Asigning express               
 
 app.use(bodyparser.urlencoded({
@@ -15,7 +16,6 @@ app.get('/', (req, res) => {
 
 
 app.use('/user',user);        //  setting router 
-
+app.use('/course',course);
 const PORT = 3000;
 app.listen(PORT,console.log(`Port is running on http://localhost:${PORT}`));
-
